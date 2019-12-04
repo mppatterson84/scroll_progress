9601236862e0e91b4cdf39ac644a97de8c5afafb
mppatterson84 <mppatterson84@gmail.com>
2019-12-04 17:07:12 -0600

    Update script.js
    When page loads and is already scrolled, update scroll bar.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 const progress = document.querySelectorAll("div.progressBar div");
 
+function progressChange (index) {
+    if (window.pageYOffset > index * window.innerHeight / 10) {
+        progress[index].classList.remove("gray");
+        progress[index].classList.add("blue");
+    } else if (window.pageYOffset <= index * window.innerHeight / 10) {
+        progress[index].classList.remove("blue");
+        progress[index].classList.add("gray");
+    }
+    return index;
+}
+
+progress.forEach( function (prog, index) {
+    window.addEventListener("load", function () {
+        progressChange(index);
+    });
+});
+
 progress.forEach( function (prog, index) {
     window.addEventListener("scroll", function () {
-        if (window.pageYOffset > index * window.innerHeight / 10) {
-            progress[index].classList.remove("gray");
-            progress[index].classList.add("blue");
-        } else if (window.pageYOffset <= index * window.innerHeight /10) {
-            progress[index].classList.remove("blue");
-            progress[index].classList.add("gray");
-        }
+        progressChange(index);
     });
 });
