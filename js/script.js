90136286b1200f2de4e5fcd18f93c78c623037d0
mppatterson84 <mppatterson84@gmail.com>
2019-12-04 16:43:00 -0600

    Add progress bar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+const progress = document.querySelectorAll("div.progressBar div");
+
+progress.forEach( function (prog, index) {
+    window.addEventListener("scroll", function () {
+        if (window.pageYOffset > index * window.innerHeight / 10) {
+            progress[index].classList.remove("gray");
+            progress[index].classList.add("blue");
+        } else if (window.pageYOffset <= index * window.innerHeight /10) {
+            progress[index].classList.remove("blue");
+            progress[index].classList.add("gray");
+        }
+    });
+});
